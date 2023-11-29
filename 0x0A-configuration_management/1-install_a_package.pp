02477562cae42bcd752de3e86c5472f9389c34c8
Tyrone Joel <tyronejoel04@gmail.com>
2023-11-29 07:41:18 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# Install a version of flask
+# Install a specific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }

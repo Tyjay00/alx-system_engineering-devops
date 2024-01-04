cfe94cfb9e07547e87ac857946f24b7ae2be5d0a
Tyrone Joel <tyronejoel04@gmail.com>
2024-01-04 13:01:24 -0800

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-A script that utilizes a REST API to retrieve and export information
-about a given employee's TODO list progress in CSV format.
-"""
+"""A script that utilizes a REST API to retrieve information."""
 
 import csv
 import json
@@ -14,30 +11,25 @@ if __name__ == "__main__":
 
     sessionReq = requests.Session()
 
-    # Extract employee ID from command line arguments
     idEmp = argv[1]
-    
-    # API URLs for TODO list and employee details
     idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
     nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
 
-    # Fetch data from the API
     employee = sessionReq.get(idURL)
     employeeName = sessionReq.get(nameURL)
 
-    # Parse JSON responses
     json_req = employee.json()
     usr = employeeName.json()['username']
 
-    # Calculate the total number of completed tasks
     totalTasks = 0
+
     for done_tasks in json_req:
         if done_tasks['completed']:
             totalTasks += 1
 
-    # Create a CSV file with employee data
     fileCSV = idEmp + '.csv'
+
     with open(fileCSV, "w", newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
+        write = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
         for i in json_req:
-            writer.writerow([idEmp, usr, i.get('completed'), i.get('title')])
+            write.writerow([idEmp, usr, i.get('completed'), i.get('title')])

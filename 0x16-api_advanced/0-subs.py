e52745687a679c09ea9be0ed67b3d1f6451c9e74
Tyrone Joel <tyronejoel04@gmail.com>
2024-01-16 08:07:42 -0800

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-"""Script to retrieve the number of subscribers for a Reddit subreddit."""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers for a given subreddit."""
-    # Reddit API endpoint for subreddit information
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    
-    # Set the user-agent header for the request
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    
-    # Send a GET request to the Reddit API
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    # Check if the subreddit exists (HTTP 404 status code)
     if response.status_code == 404:
         return 0
-    
-    # Extract the subscriber count from the JSON response
     results = response.json().get("data")
     return results.get("subscribers")

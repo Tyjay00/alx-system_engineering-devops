18bc6ecbd9657355ae1207ea5565eb2cbf3b9287
Tyrone Joel <tyronejoel04@gmail.com>
2024-01-16 08:31:34 -0800

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
-"""Script to display the titles of the top 10 hot posts on a specified Reddit subreddit."""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts for a given subreddit."""
-    # Reddit API endpoint for fetching hot posts
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    
-    # Set the user-agent header for the request
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    
-    # Parameters to limit the number of posts to 10
     params = {
         "limit": 10
     }
-    
-    # Send a GET request to the Reddit API
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    
-    # Check if the subreddit exists (HTTP 404 status code)
     if response.status_code == 404:
         print("None")
         return
-    
-    # Extract and print the titles of the top 10 hot posts
     results = response.json().get("data")
     [print(c.get("data").get("title")) for c in results.get("children")]
